ae3cfd00eb8cb0f3b895b64f6dd2cb10d22a5e0e
Krishna <124429145+KrishnaAghav07@users.noreply.github.com>
2025-05-17 23:51:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+// Slideshow logic
+const images = [
+  "images/img1.jpg",
+  "images/img2.jpg",
+  "images/img3.jpg",
+  "images/img4.jpg",
+  "images/img5.jpg",
+  "images/img6.jpg",
+  "images/img7.jpg",
+  "images/img8.jpg"
+];
+
+let index = 0;
+const slide = document.getElementById("slide");
+
+setInterval(() => {
+  index = (index + 1) % images.length;
+  slide.src = images[index];
+}, 3000);
+
+// Countdown Timer (set your date below)
+const birthday = new Date("May 20, 2025 00:00:00").getTime();
+
+const countdown = () => {
+  const now = new Date().getTime();
+  const gap = birthday - now;
+
+  const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+  const d = Math.floor(gap / day);
+  const h = Math.floor((gap % day) / hour);
+  const m = Math.floor((gap % hour) / minute);
+  const s = Math.floor((gap % minute) / second);
+
+  document.getElementById("days").innerText = d;
+  document.getElementById("hours").innerText = h;
+  document.getElementById("minutes").innerText = m;
+  document.getElementById("seconds").innerText = s;
+};
+
+setInterval(countdown, 1000);
